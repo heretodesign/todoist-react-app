a39b22d557fde4de2dd9bd9e067cdd05d99402c4
heretodesign <senwelo.works@gmail.com>
2020-01-20 10:30:37 +0800

    added firebase db config and  credentials

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from './components/layout/Header'
 import { Content } from './components/layout/Content'
 
-export const App = () => (
+const App = () => (
     <div className="App">
       <header className="App-header">
         <Header />
@@ -11,3 +11,4 @@ export const App = () => (
     </div>
   );
 
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
 const firebaseConfig = firebase.InitializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: ''
+    apiKey: "AIzaSyBZxN6eogWYUBGhQYgvAyFvsCNhecYdwjE",
+    authDomain: "todoist-tut-fcb40.firebaseapp.com",
+    databaseURL: "https://todoist-tut-fcb40.firebaseio.com",
+    projectId: "todoist-tut-fcb40",
+    storageBucket: "todoist-tut-fcb40.appspot.com",
+    messagingSenderId: "749889997590",
+    appId: "1:749889997590:web:b7883d75661295d80d9e37"
 });
 
 export { firebaseConfig as firebase };
